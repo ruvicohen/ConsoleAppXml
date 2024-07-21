24267eb91bf11043538f411680ac28125c39274b
reuven <r0583247229@gmail.com>
2024-07-21 12:32:04 +0300

    Add project files.

---
+++ ConsoleAppXml/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleAppXml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            XMLService xmlService = new XMLService("data.xml");
+            var doc = xmlService.ReadXML();
+            Console.WriteLine(@"1.Create questions
+                                2.Answer Questions");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Console.WriteLine(@"
+                                    - Type your question
+                                    - Type the answer");
+                string question = Console.ReadLine();
+                string answer = Console.ReadLine();
+                XElement element = new XElement("item1",
+                        new XElement("question", question),
+                        new XElement("answer", answer));
+                doc.;
+                var node = xmlService.GetNodeByName("Data.Item1.answer");
+                Console.WriteLine(node.InnerText);
+
+
+            }
+            //else if (choice == "2") {
+
+            //var node = xmlService.GetNodeByName("Data.Item.Answer");
+            //node.InnerText = "lll";
+            //Console.WriteLine(node.InnerText);
+            //var foo = 0;
+        }
+    }
+}
